462a2a8954f3b463eacaa2ed40050ae3b0717d51
Aayush Mandlik <aayush.mandlik@godigitaltc.com>
2025-07-10 19:25:17 +0530

    User and Admin Register and Login with jwt auth

--- backend/databases/database.py
+++ backend/databases/database.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
-import pymongo
+# import pymongo
+from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic.v1 import BaseSettings
 
 class Settings(BaseSettings):
@@ -12,7 +13,7 @@ class Settings(BaseSettings):
 
 settings = Settings()
 
-conn = MongoClient(settings.DATABASE_URL)
+conn = AsyncIOMotorClient(settings.DATABASE_URL)
 db = conn[settings.MONGO_INITDB_DATABASE]
 
 # uri = "mongodb+srv://aayushmandlik:Aayush@123@cluster0.beiyj9c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
--- backend/routes/user_routes.py
+++ backend/routes/user_routes.py
@@ -5,7 +5,7 @@ from schemas.token_schema import TokenResponse
 from passlib.context import CryptContext
 from core.security import create_access_token
 
-router = APIRouter(prefix='/api/users', tags=['User'])
+router = APIRouter(prefix='/users', tags=['User'])
 pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
 
 @router.post("/register")
@@ -17,10 +17,10 @@ async def register(user: UserRegister):
     await users_collection.insert_one({**user.dict(),"password":hashed})
     return {"message":"User Registered"}
 
-@router.post("/login")
+@router.post("/login",response_model=TokenResponse)
 async def login(user: UserLogin):
     record = await users_collection.find_one({"email":user.email})
     if not record or not pwd_context.verify(user.password,record["password"]):
         raise HTTPException(status_code=401,detail="Invalid Credentials")
     token = create_access_token({"email":record["email"], "role":"user", "name":record["name"]})
-    return {"token":token, "role":user, "email":record["email"], "name":record["name"]}
+    return {"token":token, "role":"user", "email":record["email"], "name":record["name"]}
